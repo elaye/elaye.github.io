1a6046c32847af19fd5bc0119723841931c9ddf6
elaye <genard.elie@orange.fr>
2017-03-26 12:07:35 +0100

    Clear log info

--- Rating.hs
+++ Rating.hs
@@ -8,10 +8,10 @@ import Debug.Trace (traceShow)
 
 ratingField :: String -> Context a
 ratingField name = functionField name $ \args _ -> do
-  let n = case traceShow args args of
+  let n = case args of
             [] -> 0
             (x:_) -> read x :: Int
-  return $ traceShow name $ mkRatingHtml n
+  return $ mkRatingHtml n
 
 mkRatingHtml :: Int -> String
 mkRatingHtml n = "<div class=\"rating\">" ++ rating ++ "</div>"
